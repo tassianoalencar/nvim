b9b151ce45583ccfcdc739443c8382f24a6833c8
Tassiano Alencar <tassiano.alencar@icloud.com>
2022-06-20 17:21:16 -0300

    feat: adicionar comment plugin

--- lua/settings/plugins.lua
+++ lua/settings/plugins.lua
@@ -5,47 +5,48 @@ vim.cmd([[
   augroup end
 ]])
 
-return require('packer').startup(function(use)
-    use 'wbthomason/packer.nvim'
+return require("packer").startup(function(use)
+	use("wbthomason/packer.nvim")
 
-    -- UI
-    use({ "themercorp/themer.lua" })
-    use({ "RRethy/vim-illuminate" })
-    use({ "nvim-lualine/lualine.nvim", requires = { "kyazdani42/nvim-web-devicons", opt = true } })
-    use({ "akinsho/nvim-bufferline.lua", requires = "kyazdani42/nvim-web-devicons" })
-    use({ "kazhala/close-buffers.nvim" })
-    use({ "lewis6991/gitsigns.nvim" })
-    use({ "glepnir/lspsaga.nvim", branch = "main" })
-    use({ "lukas-reineke/indent-blankline.nvim" })
+	-- UI
+	use({ "themercorp/themer.lua" })
+	use({ "RRethy/vim-illuminate" })
+	use({ "nvim-lualine/lualine.nvim", requires = { "kyazdani42/nvim-web-devicons", opt = true } })
+	use({ "akinsho/nvim-bufferline.lua", requires = "kyazdani42/nvim-web-devicons" })
+	use({ "kazhala/close-buffers.nvim" })
+	use({ "lewis6991/gitsigns.nvim" })
+	use({ "glepnir/lspsaga.nvim", branch = "main" })
+	use({ "lukas-reineke/indent-blankline.nvim" })
+	use({ "numToStr/Comment.nvim" })
 
-    -- Treesitter
-    use({ "nvim-treesitter/nvim-treesitter", run = ":TSUpdate" })
-    use({ "nvim-treesitter/nvim-treesitter-refactor" })
+	-- Treesitter
+	use({ "nvim-treesitter/nvim-treesitter", run = ":TSUpdate" })
+	use({ "nvim-treesitter/nvim-treesitter-refactor" })
 
-    -- Autopairs
-    use({ "windwp/nvim-autopairs" })
-    use({ "windwp/nvim-ts-autotag" })
+	-- Autopairs
+	use({ "windwp/nvim-autopairs" })
+	use({ "windwp/nvim-ts-autotag" })
 
-    -- Nvim Tree
-    use({ "kyazdani42/nvim-tree.lua", requires = "kyazdani42/nvim-web-devicons" })
+	-- Nvim Tree
+	use({ "kyazdani42/nvim-tree.lua", requires = "kyazdani42/nvim-web-devicons" })
 
-    -- Telescope
-    use({ "nvim-telescope/telescope.nvim", requires = { "nvim-lua/popup.nvim", "nvim-lua/plenary.nvim" } })
+	-- Telescope
+	use({ "nvim-telescope/telescope.nvim", requires = { "nvim-lua/popup.nvim", "nvim-lua/plenary.nvim" } })
 
-    -- Lsp and completion
-    use({ "williamboman/nvim-lsp-installer" })
-    use({ "neovim/nvim-lspconfig" })
-    use({ "jose-elias-alvarez/null-ls.nvim" })
-    use({ "ray-x/lsp_signature.nvim", commit = "4852d99f9511d090745d3cc1f09a75772b9e07e9" })
+	-- Lsp and completion
+	use({ "williamboman/nvim-lsp-installer" })
+	use({ "neovim/nvim-lspconfig" })
+	use({ "jose-elias-alvarez/null-ls.nvim" })
+	use({ "ray-x/lsp_signature.nvim", commit = "4852d99f9511d090745d3cc1f09a75772b9e07e9" })
 
-    -- Cmp
-    use({ "hrsh7th/cmp-nvim-lsp" })
-    use({ "hrsh7th/cmp-buffer" })
-    use({ "hrsh7th/cmp-path" })
-    use({ "hrsh7th/cmp-cmdline" })
-    use({ "hrsh7th/nvim-cmp" })
+	-- Cmp
+	use({ "hrsh7th/cmp-nvim-lsp" })
+	use({ "hrsh7th/cmp-buffer" })
+	use({ "hrsh7th/cmp-path" })
+	use({ "hrsh7th/cmp-cmdline" })
+	use({ "hrsh7th/nvim-cmp" })
 
-    -- Snippets
-    use("L3MON4D3/LuaSnip")
-    use("saadparwaiz1/cmp_luasnip")
+	-- Snippets
+	use("L3MON4D3/LuaSnip")
+	use("saadparwaiz1/cmp_luasnip")
 end)
