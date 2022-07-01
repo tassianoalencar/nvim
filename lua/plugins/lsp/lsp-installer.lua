4185ab443eb5e863d67e1202e6818cf841d83eec
Tassiano Alencar <tassiano.alencar@icloud.com>
2022-07-01 17:59:06 -0300

    refactor: remove non used configs

--- lua/plugins/lsp/lsp-installer.lua
+++ lua/plugins/lsp/lsp-installer.lua
@@ -1,46 +1,41 @@
 local status_ok, lsp_installer = pcall(require, "nvim-lsp-installer")
 if not status_ok then
-  return
+	return
 end
 
 local servers = {
-  "sumneko_lua",
-  "cssls",
-  "html",
-  "tsserver",
-  "pyright",
-  "bashls",
-  "jsonls",
-  "yamlls",
-  "intelephense"
+	"sumneko_lua",
+	"html",
+	"tsserver",
+	"intelephense",
 }
 
 lsp_installer.setup({
-  automatic_installation = true
+	automatic_installation = true,
 })
 
 local lspconfig_status_ok, lspconfig = pcall(require, "lspconfig")
 if not lspconfig_status_ok then
-  return
+	return
 end
 
 local opts = {}
 
 for _, server in pairs(servers) do
-  opts = {
-    on_attach = require("plugins.lsp.handlers").on_attach,
-    capabilities = require("plugins.lsp.handlers").capabilities,
-  }
+	opts = {
+		on_attach = require("plugins.lsp.handlers").on_attach,
+		capabilities = require("plugins.lsp.handlers").capabilities,
+	}
 
-  if server == "sumneko_lua" then
-    local sumneko_opts = require "plugins.lsp.settings.sumneko_lua"
-    opts = vim.tbl_deep_extend("force", sumneko_opts, opts)
-  end
+	if server == "intelephense" then
+		local intelephense_config = {
+			init_options = {
+				embeddedLanguages = { css = true, javascript = true },
+			},
+		}
 
-  if server == "pyright" then
-    local pyright_opts = require "plugins.lsp.settings.pyright"
-    opts = vim.tbl_deep_extend("force", pyright_opts, opts)
-  end
+		opts = vim.tbl_deep_extend("force", intelephense_config, opts)
+	end
 
-  lspconfig[server].setup(opts)
+	lspconfig[server].setup(opts)
 end
