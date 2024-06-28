99245b68cff0fbf46b158237e122957d5394cedc
Tassiano Alencar <tassiano.alencar@icloud.com>
2024-06-28 15:48:25 -0300

    refactor: change indent config

---
+++ lua/plugins/indent.lua
@@ -0,0 +1,26 @@
+return {
+    "lukas-reineke/indent-blankline.nvim",
+    main = "ibl",
+    opts = {
+      scope = { show_start = false, show_end = false },
+      indent = {
+        char = "╎",
+        tab_char = "╎"
+      },
+      exclude = {
+        filetypes = {
+          "help",
+          "alpha",
+          "dashboard",
+          "neo-tree",
+          "Trouble",
+          "trouble",
+          "lazy",
+          "mason",
+          "notify",
+          "toggleterm",
+          "lazyterm",
+        },
+      },
+    },
+  }
