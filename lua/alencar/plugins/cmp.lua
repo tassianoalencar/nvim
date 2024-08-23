494bae723902fa248247ff5413967418e29c0d41
Tassiano Alencar <tassiano.alencar@icloud.com>
2024-08-23 08:19:17 -0300

    fix: adjust lsp

--- lua/alencar/plugins/cmp.lua
+++ lua/alencar/plugins/cmp.lua
@@ -1,124 +1,103 @@
 return {
-  "hrsh7th/nvim-cmp",
-  dependencies = {
-    { "neovim/nvim-lspconfig" },
-    { "hrsh7th/cmp-nvim-lsp" },
-    { "hrsh7th/cmp-buffer" },
-    { "hrsh7th/cmp-path" },
-    { "hrsh7th/cmp-cmdline" },
-    { "L3MON4D3/LuaSnip",        version = "v2.*", build = "make install_jsregexp" },
-    { "saadparwaiz1/cmp_luasnip" },
-    { "hrsh7th/cmp-nvim-lua" },
-    { "Exafunction/codeium.nvim" },
-  },
-  config = function()
-    local cmp = require("cmp")
-    local luasnip = require("luasnip")
-    -- local codeium = require("codeium")
-    -- codeium.setup({})
+	"hrsh7th/nvim-cmp",
+	dependencies = {
+		{ "neovim/nvim-lspconfig" },
+		{ "hrsh7th/cmp-nvim-lsp" },
+		{ "hrsh7th/cmp-buffer" },
+		{ "hrsh7th/cmp-path" },
+		{ "hrsh7th/cmp-cmdline" },
+		{ "L3MON4D3/LuaSnip", version = "v2.*", build = "make install_jsregexp" },
+		{ "saadparwaiz1/cmp_luasnip" },
+		{ "hrsh7th/cmp-nvim-lua" },
+	},
+	config = function()
+		local cmp = require("cmp")
+		local luasnip = require("luasnip")
 
-    local function border(hl_name)
-      return {
-        { "┌", hl_name },
-        { "─", hl_name },
-        { "┐", hl_name },
-        { "│", hl_name },
-        { "┘", hl_name },
-        { "─", hl_name },
-        { "└", hl_name },
-        { "│", hl_name },
-      }
-    end
+		local function border(hl_name)
+			return {
+				{ "┌", hl_name },
+				{ "─", hl_name },
+				{ "┐", hl_name },
+				{ "│", hl_name },
+				{ "┘", hl_name },
+				{ "─", hl_name },
+				{ "└", hl_name },
+				{ "│", hl_name },
+			}
+		end
 
-    -- Define HL Groups
-    cmp.setup({
-      experimental = {
-        native_menu = false,
-        ghost_text = { enabled = true, hl_group = "MagoGhostText" },
-      },
-      snippet = {
-        expand = function(args)
-          luasnip.lsp_expand(args.body)
-        end,
-      },
-      window = {
-        completion = cmp.config.window.bordered(),
-        documentation = {
-          border = border "CmpDocBorder",
-          winhighlight = "Normal:CmpDoc",
-        }
-        -- documentation = cmp.config.window.bordered(),
-      },
-      sources = cmp.config.sources({
-        { name = "nvim_lsp" },
-        { name = "codeium" },
-        { name = "nvim_lua" },
-        { name = "luasnip" },
-      }),
-      mapping = {
-        ["<C-j>"] = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Insert }),
-        ["<C-k>"] = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Insert }),
-        ["<C-d>"] = cmp.mapping.scroll_docs(-4),
-        ["<C-f>"] = cmp.mapping.scroll_docs(4),
-        ["<C-Space>"] = cmp.mapping.complete(),
-        ["<C-e>"] = cmp.mapping.close(),
-        ['<CR>'] = cmp.mapping(function(fallback)
-          if cmp.visible() then
-            if luasnip.expandable() then
-              luasnip.expand()
-            else
-              cmp.confirm({
-                select = true,
-              })
-            end
-          else
-            fallback()
-          end
-        end),
+		-- Define HL Groups
+		cmp.setup({
+			-- experimental = {
+			-- 	native_menu = false,
+			-- 	ghost_text = { enabled = false, hl_group = "LspCodeLens" },
+			-- },
+			snippet = {
+				expand = function(args)
+					luasnip.lsp_expand(args.body)
+				end,
+			},
+			window = {
+				completion = cmp.config.window.bordered(),
+				documentation = {
+					border = border("CmpDocBorder"),
+					winhighlight = "Normal:CmpDoc",
+				},
+			},
+			sources = cmp.config.sources({
+				{ name = "nvim_lsp" },
+				{ name = "nvim_lua" },
+				{ name = "luasnip" },
+			}),
+			mapping = {
+				["<C-j>"] = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Insert }),
+				["<C-k>"] = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Insert }),
+				["<C-d>"] = cmp.mapping.scroll_docs(-4),
+				["<C-f>"] = cmp.mapping.scroll_docs(4),
+				["<C-Space>"] = cmp.mapping.complete(),
+				["<C-e>"] = cmp.mapping.close(),
+				["<CR>"] = cmp.mapping(function(fallback)
+					if cmp.visible() then
+						if luasnip.expandable() then
+							luasnip.expand()
+						else
+							cmp.confirm({
+								select = true,
+							})
+						end
+					else
+						fallback()
+					end
+				end),
 
-        ["<Tab>"] = cmp.mapping(function(fallback)
-          if cmp.visible() then
-            cmp.select_next_item()
-          elseif luasnip.locally_jumpable(1) then
-            luasnip.jump(1)
-          else
-            fallback()
-          end
-        end, { "i", "s" }),
+				["<Tab>"] = cmp.mapping(function(fallback)
+					if cmp.visible() then
+						cmp.select_next_item()
+					elseif luasnip.locally_jumpable(1) then
+						luasnip.jump(1)
+					else
+						fallback()
+					end
+				end, { "i", "s" }),
 
-        ["<S-Tab>"] = cmp.mapping(function(fallback)
-          if cmp.visible() then
-            cmp.select_prev_item()
-          elseif luasnip.locally_jumpable(-1) then
-            luasnip.jump(-1)
-          else
-            fallback()
-          end
-        end, { "i", "s" }),
+				["<S-Tab>"] = cmp.mapping(function(fallback)
+					if cmp.visible() then
+						cmp.select_prev_item()
+					elseif luasnip.locally_jumpable(-1) then
+						luasnip.jump(-1)
+					else
+						fallback()
+					end
+				end, { "i", "s" }),
+			},
+		})
 
-        -- ["<CR>"] = cmp.mapping.confirm({ select = true }),
-        -- ['<Tab>'] = cmp.mapping(function(fallback)
-        --   if luasnip.jumpable() then
-        --     luasnip.jump(1)
-        --   else
-        --     fallback()
-        --   end
-        -- end, { 'i', 's' }),
-        -- ['<S-Tab>'] = cmp.mapping(function(fallback)
-        --   if luasnip.jumpable(-1) then
-        --     luasnip.jump(-1)
-        --   else
-        --     fallback()
-        --   end
-        -- end, { 'i', 's' }),
-      },
-    })
-
-    cmp.setup.cmdline({ "/", "?" }, {
-      mapping = cmp.mapping.preset.cmdline(),
-      sources = {
-        { name = "buffer" },
-      },
-    })
-  end,
+		cmp.setup.cmdline({ "/", "?" }, {
+			mapping = cmp.mapping.preset.cmdline(),
+			sources = {
+				{ name = "buffer" },
+			},
+		})
+	end,
 }
--- lua/alencar/plugins/lsp.lua
+++ lua/alencar/plugins/lsp.lua
@@ -1,5 +1,9 @@
 local api = vim.api
 
+local function lsp_show_diagnostics()
+	vim.diagnostic.open_float({ border = "single" })
+end
+
 local servers = {
 	pyright = {},
 	tsserver = {},
@@ -60,15 +64,21 @@ api.nvim_create_autocmd("LspAttach", {
 			vim.keymap.set("n", keys, func, { buffer = event.buf, desc = "LSP: " .. desc })
 		end
 
-		map("gd", require("telescope.builtin").lsp_definitions, "[G]oto [D]efinition")
-		map("gr", require("telescope.builtin").lsp_references, "[G]oto [R]eferences")
-		map("<leader>D", require("telescope.builtin").lsp_type_definitions, "Type [D]efinition")
-		map("gI", require("telescope.builtin").lsp_implementations, "[G]oto [I]mplementation")
+		local builtin = require("telescope.builtin")
+
+		local opts = { noremap = true, silent = true }
+
+		vim.keymap.set("n", "<leader>cd", lsp_show_diagnostics, opts)
+
+		map("gd", builtin.lsp_definitions, "[G]oto [D]efinition")
+		map("gr", builtin.lsp_references, "[G]oto [R]eferences")
+		map("gI", builtin.lsp_implementations, "[G]oto [I]mplementation")
+		map("gD", vim.lsp.buf.declaration, "[G]oto [D]eclaration")
 		map("<leader>ds", require("telescope.builtin").lsp_document_symbols, "[D]ocument [S]ymbols")
 		map("<leader>ws", require("telescope.builtin").lsp_dynamic_workspace_symbols, "[W]orkspace [S]ymbols")
 		map("<leader>rn", vim.lsp.buf.rename, "[R]e[n]ame")
 		map("<leader>ca", vim.lsp.buf.code_action, "[C]ode [A]ction")
-		map("gD", vim.lsp.buf.declaration, "[G]oto [D]eclaration")
+		map("<leader>D", require("telescope.builtin").lsp_type_definitions, "Type [D]efinition")
 
 		local client = vim.lsp.get_client_by_id(event.data.client_id)
 
