2ef539e9943ceba818995de4e8ede19f8552fb28
Tassiano Alencar <tassiano.alencar@icloud.com>
2024-08-30 09:30:59 -0300

    refactor: show virtual blame text

--- lua/alencar/plugins/gitsings.lua
+++ lua/alencar/plugins/gitsings.lua
@@ -1,15 +1,15 @@
 return {
 	"lewis6991/gitsigns.nvim",
-	config = function()
-		require("gitsigns").setup({
-			signs = {
-				add = { text = "+" },
-				change = { text = "~" },
-				delete = { text = "-" },
-				topdelete = { text = "-" },
-				changedelete = { text = "~" },
-				untracked = { text = "┆" },
-			},
-		})
-	end,
+	event = "BufRead",
+	opts = {
+		numhl = true,
+		current_line_blame = true,
+		current_line_blame_opts = {
+			delay = 500,
+		},
+	},
+	keys = {
+		{ "<leader>gb", "<cmd>Gitsigns blame<cr>", desc = "Git blame" },
+		{ "<leader>gl", "<cmd>Gitsigns blame_line<cr>", desc = "Git blame line" },
+	},
 }
