a29986269c7e6deaa1ba4acfc4bd617376299216
Tassiano Alencar <tassiano.alencar@icloud.com>
2024-09-20 18:40:06 -0300

    refactor: remove unused plugins

--- lua/alencar/plugins/neo-tree.lua
+++ lua/alencar/plugins/neo-tree.lua
@@ -1,17 +1,17 @@
 return {
-  'nvim-neo-tree/neo-tree.nvim',
-  version = '*',
-  cmd = 'Neotree',
-  keys = {
-    { '<leader>e', ':Neotree reveal<CR>', desc = 'NeoTree reveal' },
-  },
-  opts = {
-    filesystem = {
-      window = {
-        mappings = {
-          ['\\'] = 'close_window',
-        },
-      },
-    },
-  },
+	"nvim-neo-tree/neo-tree.nvim",
+	version = "*",
+	cmd = "Neotree",
+	keys = {
+		{ "<leader>e", ":Neotree toggle<CR>", desc = "NeoTree reveal" },
+	},
+	opts = {
+		filesystem = {
+			window = {
+				mappings = {
+					["\\"] = "close_window",
+				},
+			},
+		},
+	},
 }
