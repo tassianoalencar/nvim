f0ac99cefe342dbcb182f35ef4130d22e86da289
Tassiano Alencar <tassiano.alencar@icloud.com>
2024-09-20 15:47:55 -0300

    refactor: change barbar config

--- lua/alencar/plugins/barbar.lua
+++ lua/alencar/plugins/barbar.lua
@@ -4,12 +4,12 @@ return {
 		"lewis6991/gitsigns.nvim", -- OPTIONAL: for git status
 		"nvim-tree/nvim-web-devicons", -- OPTIONAL: for file icons
 	},
-	init = function()
-		vim.g.barbar_auto_setup = true
+	config = function()
+		vim.g.barbar_auto_setup = false
+
+		require("barbar").setup()
+
+		vim.keymap.set("n", "<S-h>", "<cmd>BufferPrevious<cr>", {})
+		vim.keymap.set("n", "<S-l>", "<cmd>BufferNext<cr>", {})
 	end,
-	opts = {},
-	keys = {
-		{ "<S-h>", "<cmd>BufferPrevious<cr>", desc = "Buffer Previous" },
-		{ "<S-l>", "<cmd>BufferNext<cr>", desc = "Buffer Netxt" },
-	},
 }
