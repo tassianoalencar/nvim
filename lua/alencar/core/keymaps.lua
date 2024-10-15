10e06248391438fe84de45e49c5235b5deb6f073
Tassiano Alencar <tassiano.alencar@icloud.com>
2024-10-15 17:44:25 -0300

    refactor: change keymaps

--- lua/alencar/core/keymaps.lua
+++ lua/alencar/core/keymaps.lua
@@ -1,22 +1,17 @@
 local keymap = vim.keymap.set
 
-keymap('n', '<Esc>', '<cmd>nohlsearch<CR>')
-keymap('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostic [Q]uickfix list' })
-keymap('n', '<C-h>', '<C-w><C-h>', { desc = 'Move focus to the left window' })
-keymap('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right window' })
-keymap('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
-keymap('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
+keymap("n", "<Esc>", "<cmd>nohlsearch<CR>")
+keymap("n", "<leader>q", vim.diagnostic.setloclist, { desc = "Open diagnostic [Q]uickfix list" })
+keymap("n", "<C-h>", "<C-w><C-h>", { desc = "Move focus to the left window" })
+keymap("n", "<C-l>", "<C-w><C-l>", { desc = "Move focus to the right window" })
+keymap("n", "<C-j>", "<C-w><C-j>", { desc = "Move focus to the lower window" })
+keymap("n", "<C-k>", "<C-w><C-k>", { desc = "Move focus to the upper window" })
 
+keymap("n", "<C-d>", "<C-d>zz")
+keymap("n", "<C-u>", "<C-u>zz")
 
-keymap('n', '<C-d>', '<C-d>zz')
-keymap('n', '<C-u>', '<C-u>zz')
-
-
-
--- Exit terminal mode in the builtin terminal with a shortcut that is a bit easier
--- for people to discover. Otherwise, you normally need to press <C-\><C-n>, which
--- is not what someone will guess without a bit more experience.
---
--- NOTE: This won't work in all terminal emulators/tmux/etc. Try your own mapping
--- or just use <C-\><C-n> to exit terminal mode
-vim.keymap.set('t', '<Esc><Esc>', '<C-\\><C-n>', { desc = 'Exit terminal mode' })
+-- Resize windows with Ctrl+Shift+Arrow
+keymap("n", "<C-S-Up>", "<C-w>+", { desc = "Resize window up" })
+keymap("n", "<C-S-Down>", "<C-w>-", { desc = "Resize window down" })
+keymap("n", "<C-S-Left>", "<C-w><", { desc = "Resize window left" })
+keymap("n", "<C-S-Right>", "<C-w>>", { desc = "Resize window right" })
