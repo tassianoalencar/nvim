e60c898e8feda9215f2857de0fd82a653f10b421
Tassiano Alencar <tassiano.alencar@icloud.com>
2025-02-10 14:25:27 -0300

    fix: remove dap

--- init.lua
+++ init.lua
@@ -17,7 +17,6 @@ vim.opt.rtp:prepend(lazypath)
 require('lazy').setup {
   spec = {
     { import = 'plugins.lsp' },
-    { import = 'plugins.dap' },
     { import = 'plugins' },
   },
   checker = {
